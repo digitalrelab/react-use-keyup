6ba1c49d9a998e9fa82a48f9721674029e2bd970
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2019-05-01 21:24:02 -0300

    fix: Update keycode import statement to avoid rollup conflicts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import { useEffect } from "react"
 
-/**
- * CommonJS modules are not compatible with Rollup. In other words,
- * specifying the exact path is required as per
- * https://github.com/Microsoft/TypeScript/issues/30472#issuecomment-474453292.
- */
-const keycode = require("../node_modules/keycode/index")
+// @ts-ignore
+// Ignoring this for now thanks to Rollup incompatibility with CommonJS.
+import keycode from "keycode"
 
 /**
  * useKeyUp is a React hook that binds keyup events.
