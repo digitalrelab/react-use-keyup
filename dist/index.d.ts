6d08ede67a0b4a201490349452d900ed6d235c29
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2019-07-08 13:24:25 -0300

    refactor: 💡 remove number spoiler

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-export declare function useKeyUp(key: TAvailableKeys | string | number, onKeyUp: TOnKeyUp): void;
+export declare function useKeyUp(key: TAvailableKeys | string, onKeyUp: TOnKeyUp): void;
 declare type TOnKeyUp = (keyboardEvent: KeyboardEvent) => void;
 declare type TAvailableKeys = "esc" | "enter";
 export {};
